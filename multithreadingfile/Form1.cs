7c3ee1a37685866a0eb5be096aced1bf88b6ce5b
Shahzaib Hassan <shahzaib.hassan@ghostsoftware.local>
2021-05-23 17:17:38 +0500

    Add project files.

---
+++ multithreadingfile/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace multithreadingfile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileInfo file = new FileInfo(@"C:\Users\shahzaib.hassan\Desktop\File\file.text");
+            StreamWriter writer =  file.CreateText();
+            writer.WriteLine(textBox1.Text);
+            writer.Close();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            FileInfo fil = new FileInfo(@"C:\Users\shahzaib.hassan\Desktop\File\Myfile.text");
+           
+            StreamWriter writer = fil.CreateText();
+            writer.WriteLine(textBox1.Text);
+            writer.Close();
+
+        }
+    }
+}
---
+++ timer/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace timer
+{
+   
+        public class ThrdTime
+        {
+            static int countdown = 10;
+            static System.Threading.Timer timer;
+            static public void Main()
+            {
+                // Create the timer callback delegate.
+                System.Threading.TimerCallback cb = new System.Threading.
+                TimerCallback(ProcessTimerEvent);
+                // Create the object for the timer.
+                clsTime time = new clsTime();
+                // Create the timer. It is autostart, so creating the timer will start it.
+                timer = new System.Threading.Timer(cb, time, 4000, 1000);
+                // Blessed are those who wait.
+                MessageBox.Show("Waiting for countdown", "Text");
+            }
+            // Callback method for the timer. The only parameter is the object you
+            // ed when you created the timer object.
+            private static void ProcessTimerEvent(object obj)
+            {
+                --countdown;
+                // If countdown is complete, exit the program.
+                if (countdown == 0)
+                {
+                    timer.Dispose();
+                    Environment.Exit(0);
+                }
+                string str = "";
+                // Cast the obj argument to clsTime.
+                if (obj is clsTime)
+                {
+                    clsTime time = (clsTime)obj;
+                    str = time.GetTimeString();
+                }
+                str += "\r\nCountdown = " + countdown;
+                MessageBox.Show(str, "Timer Thread");
+            }
+        }
+        // Define a class to use as the object argument for the timer.
+        class clsTime
+        {
+            public string GetTimeString()
+            {
+                string str = DateTime.Now.ToString();
+                int index = str.IndexOf(" ");
+                return (str.Substring(index + 1));
+            }
+        }
+    }
+
